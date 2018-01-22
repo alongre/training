39b23ee1e80c0f145bd59d473854603d85458e1f
alongre <alongre@gmail.com>
2018-01-22 06:46:01 +0200

    pig game

--- the-complete-javascript-course/4-DOM-Pig-Game/app.js
+++ the-complete-javascript-course/4-DOM-Pig-Game/app.js
@@ -8,3 +8,40 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+const diceObject = document.querySelector('.dice');
+diceObject.style.display = 'none';
+var scores = [0,0];
+var roundScore = 0;
+var activePlayer = 0;
+
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+const playersDOM = [];
+playersDOM[0] = document.getElementById('current-0');
+playersDOM[1] = document.getElementById('current-1');
+playersDOM[0].textContent = 0;
+playersDOM[1].textContent = 0;
+
+
+changePlayer = () => {
+    scores[activePlayer] = 0;
+    playersDOM[activePlayer].textContent = 0;
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    diceObject.style.display = 'none';
+}
+
+addScore = (dice) => {
+    scores[activePlayer] += dice;
+    playersDOM[activePlayer].textContent = scores[activePlayer];
+}
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+    diceObject.style.display = 'block';
+    const dice = Math.floor(Math.random() * 6 + 1);
+    diceObject.src = 'dice-' + dice + '.png'; 
+    dice > 1 ? addScore(dice) : changePlayer();
+   
+})
